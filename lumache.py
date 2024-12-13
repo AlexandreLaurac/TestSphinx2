1d75e2e434815a8f055646cdf7ca26d45cf762c3
AlexandreLaurac <alexandre_laurac@yahoo.fr>
2024-12-13 15:48:20 +0100

    Modifying a file on master branch to generate documentation

--- lumache.py
+++ lumache.py
@@ -13,8 +13,8 @@ def get_random_ingredients(kind=None):
     return ["eggs", "bacon", "spam"]
 
 def hello_world():
-    """The classical "Hello, World!" function"""
-    print("Hello, World!")
+    """The classical "Hello, world!" function"""
+    print("Hello, world!")
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid"""
